99815774edd618a6b9669511f730e984ecd3c7cd
klburks <90107031+klburks@users.noreply.github.com>
2021-09-19 15:52:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+//Question 1 & 2
+let myNumber = 5;
+
+//Question 3
+let myOtherNumber = 5;
+
+//Question 4
+let myTotal= myNumber * myOtherNumber;
+
+//Question 5
+let myName = 'Kristy Burks';
+let myCombo = String(myNumber + myOtherNumber);
+
+//Question 6
+let myOtherCombo = myNumber + myOtherNumber;
+
+//Question 7
+let myRemainder = myNumber/5;
+
+//Question 8
+let i = 2;
+
+//Question 9
+let int = 4;
+
+//Question 10
+myBoolean = 2 > 3;
+
+
+
